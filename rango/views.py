48c5f10d8028d4f1c8656e73317f49b36882a191
Darryl Lynch <2671742l@student.gla.ac.uk>
2023-02-06 22:11:26 +0000

    Category mapping

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by("-likes")[:5]
@@ -15,3 +16,15 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by name'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    try:
+        context_dict = {}
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
