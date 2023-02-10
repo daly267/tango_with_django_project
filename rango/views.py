43a1d4515bfbc0260b8e40703e993511a0107c8d
Darryl Lynch <2671742l@student.gla.ac.uk>
2023-02-10 01:24:39 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -63,7 +63,6 @@ def add_page(request, category_name_slug):
 
     if request.method == 'POST':
         form = PageForm(request.POST)
-        print(":b:ost")
         if form.is_valid():
             if category:
                 page = form.save(commit=False)
